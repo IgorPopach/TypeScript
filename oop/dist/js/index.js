c80cd8bfb883f8450b77ea9c914e84edce69db2e
Igor Popach <igorpopach@step.itstep.rv.ua>
2019-01-28 21:00:56 +0200

    ++

--- oop/dist/js/index.js
+++ oop/dist/js/index.js
@@ -1,18 +1,19 @@
 "use strict";
 class localStore {
-    writeToStorage(newDog) {
+    writeToStorage(newAnimal, type) {
         const myStorage = window.localStorage;
-        myStorage.setItem('Dogs', JSON.stringify(newDog));
+        myStorage.setItem(type, JSON.stringify(newAnimal));
     }
-    getFromStorage() {
+    getFromStorage(type) {
         const myStorage = window.localStorage;
-        const dogs = JSON.parse(myStorage.getItem('Dogs'));
-        return dogs;
+        const animals = JSON.parse(myStorage.getItem(type));
+        return animals;
     }
 }
 class Dog extends localStore {
     constructor(name, breed, age) {
         super();
+        this.type = "Dog";
         this.name = name;
         this.breed = breed;
         this.age = age;
@@ -29,11 +30,24 @@ class Dog extends localStore {
     getAge() {
         return this.age;
     }
+    getType() {
+        return this.type;
+    }
     setAge(newAge) {
         if (newAge < 80 || newAge > 3)
             this.age = newAge;
     }
+    static test() {
+        console.log("Dog static");
+    }
+    showInfo() {
+        console.log(`${this.name} ${this.type} ${this.breed} ${this.age}`);
+    }
+    die() {
+        console.log(`${this.name} DIE!!!!!!o_0`);
+    }
 }
+// Dog.test();
 // let Bobik = new Dog("Bobik", "Tax", 3);
 // Bobik.showDog();
 // console.log('Age',Bobik.getAge());
@@ -42,11 +56,15 @@ class Dog extends localStore {
 class HunterDog extends Dog {
     constructor(name, breed, age, power) {
         super(name, breed, age);
+        this.type = "Hunter Dog";
         this.power = power;
     }
     hunt() {
         this.power -= 10;
         console.log(`Hunting, power ${this.power}`);
+        if (this.power <= 0) {
+            this.die();
+        }
     }
     getPower() {
         return this.power;
@@ -58,12 +76,88 @@ class HunterDog extends Dog {
         console.log('static');
     }
 }
-let Bimbo = new HunterDog('Bimbo', "Boxedr", 5, 100);
-Bimbo.showDog();
-Bimbo.hunt();
-Bimbo.setAge(10);
-Bimbo.showDog();
-HunterDog.static();
+class Cat extends localStore {
+    constructor(name, breed, age) {
+        super();
+        this.type = "Cat";
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+    showDog() {
+        console.log(`Name ${this.name} age - ${this.age}`);
+    }
+    getName() {
+        return this.name;
+    }
+    getBreed() {
+        return this.breed;
+    }
+    getAge() {
+        return this.age;
+    }
+    getType() {
+        return this.type;
+    }
+    setAge(newAge) {
+        if (newAge < 80 || newAge > 3)
+            this.age = newAge;
+    }
+    static test() {
+        console.log("Dog static");
+    }
+    showInfo() {
+        console.log(`${this.name} ${this.type} ${this.breed} ${this.age}`);
+    }
+    outdoor() {
+        console.log(`Cat ${this.name} want to go outdoor!!!`);
+    }
+    die() {
+        console.log(`${this.name} DIE!!!!!!o_0`);
+    }
+}
+class homeCat extends Cat {
+    constructor(name, breed, age, active) {
+        super(name, breed, age);
+        this.type = "Home cat";
+        this.active = active;
+    }
+    sleeping() {
+        this.active += 20;
+        console.log(`Sleeping, active ${this.active}`);
+        if (this.active >= 100) {
+            this.outdoor();
+        }
+    }
+    getActive() {
+        return this.active;
+    }
+    showCat() {
+        console.log(`Name ${this.name} age - ${this.age}, active => ${this.active}`);
+    }
+}
+let Bimbo = new HunterDog('Bimbo', "Boxedr", 12, 20);
+// Bimbo.showDog();
+// Bimbo.hunt();
+// Bimbo.showDog();
+// Bimbo.hunt();
+// Bimbo.showDog();
+// Bimbo.hunt();
+// Bimbo.showDog();
+// Bimbo.die();
+// Bimbo.setAge(10);
+// Bimbo.showDog();
+// HunterDog.static();
+function Gen(data) {
+    return data;
+}
+console.log(Gen('test').length);
+console.log(Gen(10).length);
+function genericType(data) {
+    return data;
+}
+console.log('genericType', genericType('test').length);
+console.log('genericType', genericType(10).length);
 function Add() {
     const inputA = document.getElementsByTagName("input")[0];
     const a = inputA.value;
@@ -73,22 +167,41 @@ function Add() {
     const c = parseInt(inputC.value);
     const inputD = document.getElementsByTagName("input")[3];
     const d = parseInt(inputD.value);
-    console.log('a, b, c, d', a, b, c, d);
-    if (a === '' || b === '' || c == NaN || d == NaN) {
+    if (a === '' || b === '' || inputC.value == '' || inputD.value == '') {
         alert("PLease input all values!");
     }
     else {
-        const newObjectDog = new HunterDog(a, b, c, d);
-        const newDog = [];
-        newDog.push(newObjectDog.getName(), newObjectDog.getBreed(), newObjectDog.getAge().toString(), newObjectDog.getPower().toString());
-        let Store = newObjectDog.getFromStorage();
-        if (Store !== null) {
-            Store.push(newDog);
-            console.log('typeof(Store)', typeof (Store), Store);
-            newObjectDog.writeToStorage(Store);
-        }
-        else {
-            newObjectDog.writeToStorage([newDog]);
+        const elem = document.getElementById("choose-type");
+        const choose = elem.options[elem.selectedIndex].value;
+        switch (choose) {
+            case "dog":
+                const newObjectDog = new HunterDog(a, b, c, d);
+                const newDog = [];
+                newDog.push(newObjectDog.getName(), newObjectDog.getBreed(), newObjectDog.getAge().toString(), newObjectDog.getPower().toString());
+                let DogStore = newObjectDog.getFromStorage(newObjectDog.getType());
+                if (DogStore !== null) {
+                    DogStore.push(newDog);
+                    console.log('typeof(Store)', typeof (DogStore), DogStore);
+                    newObjectDog.writeToStorage(DogStore, newObjectDog.getType());
+                }
+                else {
+                    newObjectDog.writeToStorage([newDog], newObjectDog.getType());
+                }
+                break;
+            case "cat":
+                const newObjectCat = new HunterDog(a, b, c, d);
+                const newCat = [];
+                newCat.push(newObjectCat.getName(), newObjectCat.getBreed(), newObjectCat.getAge().toString(), newObjectCat.getPower().toString());
+                let CatStore = newObjectCat.getFromStorage(newObjectCat.getType());
+                if (CatStore !== null) {
+                    CatStore.push(newCat);
+                    console.log('typeof(CatStore)', typeof (CatStore), CatStore);
+                    newObjectCat.writeToStorage(CatStore, newObjectCat.getType());
+                }
+                else {
+                    newObjectCat.writeToStorage([newCat], newObjectCat.getType());
+                }
+                break;
         }
     }
 }
@@ -103,4 +216,6 @@ function ShowLocal() {
         newP.appendChild(document.createTextNode(text));
         tag.appendChild(newP);
     });
+}
+function getAnimal() {
 }
--- oop/index.ts
+++ oop/index.ts
@@ -1,19 +1,28 @@
+interface IAnimal {
+    name: string;
+    type: string;
+    age: number;
+    showInfo(): void;
+    die(): void
+}
+
 abstract class localStore {
-    public writeToStorage(newDog: string[][]): void {
+    public writeToStorage(newAnimal: string[][], type: string): void {
         const myStorage = window.localStorage;
-        myStorage.setItem('Dogs', JSON.stringify(newDog));
+        myStorage.setItem(type, JSON.stringify(newAnimal));
     }
-    public getFromStorage() {
+    public getFromStorage(type: string) {
         const myStorage: any = window.localStorage;
-        const dogs: any = JSON.parse(myStorage.getItem('Dogs'));
-        return dogs;
+        const animals: any = JSON.parse(myStorage.getItem(type));
+        return animals;
     }
 }
 
-abstract class Dog extends localStore {
-    protected name: string;
+abstract class Dog extends localStore implements IAnimal {
+    name: string;
+    type: string = "Dog";
+    age: number;
     protected breed: string;
-    protected age: number;
     constructor(name: string, breed: string, age: number) {
         super();
         this.name = name;
@@ -32,11 +41,25 @@ abstract class Dog extends localStore {
     public getAge(): number {
         return this.age;
     }
+    public getType(): string {
+        return this.type;
+    }
     public setAge(newAge: number): void {
         if (newAge < 80 || newAge > 3)
             this.age = newAge;
     }
+    public static test(): void {
+        console.log("Dog static")
+    }
+    showInfo(): void {
+        console.log(`${this.name} ${this.type} ${this.breed} ${this.age}`);
+    }
+    die(): void {
+        console.log(`${this.name} DIE!!!!!!o_0`);
+    }
 }
+
+// Dog.test();
 
 // let Bobik = new Dog("Bobik", "Tax", 3);
 
@@ -46,14 +69,18 @@ abstract class Dog extends localStore {
 // console.log('newAge',Bobik.getAge());
 
 class HunterDog extends Dog {
+    type: string = "Hunter Dog";
     private power: number;
     constructor(name: string, breed: string, age: number, power: number) {
         super(name, breed, age);
         this.power = power;
     }
     public hunt(): void {
-        this.power -= 10
+        this.power -= 10;
         console.log(`Hunting, power ${this.power}`)
+        if (this.power <= 0) {
+            this.die();
+        }
     }
     public getPower(): number {
         return this.power;
@@ -64,15 +91,100 @@ class HunterDog extends Dog {
     public static static(): void {
         console.log('static');
     }
-
 }
 
-let Bimbo = new HunterDog('Bimbo', "Boxedr", 5, 100);
-Bimbo.showDog();
-Bimbo.hunt();
-Bimbo.setAge(10);
-Bimbo.showDog();
-HunterDog.static();
+abstract class Cat extends localStore implements IAnimal {
+    name: string;
+    type: string = "Cat";
+    age: number;
+    protected breed: string;
+    constructor(name: string, breed: string, age: number) {
+        super();
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+    public showDog(): void {
+        console.log(`Name ${this.name} age - ${this.age}`);
+    }
+    public getName(): string {
+        return this.name;
+    }
+    public getBreed(): string {
+        return this.breed;
+    }
+    public getAge(): number {
+        return this.age;
+    }
+    public getType(): string {
+        return this.type;
+    }
+    public setAge(newAge: number): void {
+        if (newAge < 80 || newAge > 3)
+            this.age = newAge;
+    }
+    public static test(): void {
+        console.log("Dog static")
+    }
+    showInfo(): void {
+        console.log(`${this.name} ${this.type} ${this.breed} ${this.age}`);
+    }
+    outdoor(): void {
+        console.log(`Cat ${this.name} want to go outdoor!!!`);
+    }
+    die(): void {
+        console.log(`${this.name} DIE!!!!!!o_0`);
+    }
+}
+
+class homeCat extends Cat {
+    type: string = "Home cat";
+    private active: number;
+    constructor(name: string, breed: string, age: number, active: number) {
+        super(name, breed, age);
+        this.active = active;
+    }
+    public sleeping(): void {
+        this.active += 20;
+        console.log(`Sleeping, active ${this.active}`)
+        if (this.active >= 100) {
+            this.outdoor();
+        }
+    }
+    public getActive(): number {
+        return this.active;
+    }
+    public showCat(): void {
+        console.log(`Name ${this.name} age - ${this.age}, active => ${this.active}`);
+    }
+}
+
+let Bimbo = new HunterDog('Bimbo', "Boxedr", 12, 20);
+
+// Bimbo.showDog();
+// Bimbo.hunt();
+// Bimbo.showDog();
+// Bimbo.hunt();
+// Bimbo.showDog();
+// Bimbo.hunt();
+// Bimbo.showDog();
+// Bimbo.die();
+// Bimbo.setAge(10);
+// Bimbo.showDog();
+// HunterDog.static();
+
+function Gen(data: any): any {
+    return data;
+}
+console.log(Gen('test').length);
+console.log(Gen(10).length);
+
+function genericType<T>(data: T): T {
+    return data
+}
+
+console.log('genericType', genericType('test').length);
+console.log('genericType', genericType(10).length);
 
 function Add(): void {
     const inputA: HTMLInputElement = <HTMLInputElement>document.getElementsByTagName("input")[0];
@@ -83,20 +195,38 @@ function Add(): void {
     const c: number = parseInt(inputC.value);
     const inputD: HTMLInputElement = <HTMLInputElement>document.getElementsByTagName("input")[3];
     const d: number = parseInt(inputD.value);
-    console.log('a, b, c, d', a, b, c, d)
-    if (a === '' || b === '' || c == NaN || d == NaN) {
+    if (a === '' || b === '' || inputC.value == '' || inputD.value == '') {
         alert("PLease input all values!")
     } else {
-        const newObjectDog = new HunterDog(a, b, c, d);
-        const newDog: string[] = [];
-        newDog.push(newObjectDog.getName(), newObjectDog.getBreed(), newObjectDog.getAge().toString(), newObjectDog.getPower().toString());
-        let Store: any | null = newObjectDog.getFromStorage();
-        if (Store !== null) {
-            Store.push(newDog);
-            console.log('typeof(Store)', typeof (Store), Store)
-            newObjectDog.writeToStorage(Store)
-        } else {
-            newObjectDog.writeToStorage([newDog])
+        const elem: any = document.getElementById("choose-type");
+        const choose: any = elem.options[elem.selectedIndex].value;
+        switch (choose) {
+            case "dog":
+                const newObjectDog = new HunterDog(a, b, c, d);
+                const newDog: string[] = [];
+                newDog.push(newObjectDog.getName(), newObjectDog.getBreed(), newObjectDog.getAge().toString(), newObjectDog.getPower().toString());
+                let DogStore: any | null = newObjectDog.getFromStorage(newObjectDog.getType());
+                if (DogStore !== null) {
+                    DogStore.push(newDog);
+                    console.log('typeof(Store)', typeof (DogStore), DogStore)
+                    newObjectDog.writeToStorage(DogStore, newObjectDog.getType())
+                } else {
+                    newObjectDog.writeToStorage([newDog], newObjectDog.getType())
+                }
+                break;
+            case "cat":
+                const newObjectCat = new HunterDog(a, b, c, d);
+                const newCat: string[] = [];
+                newCat.push(newObjectCat.getName(), newObjectCat.getBreed(), newObjectCat.getAge().toString(), newObjectCat.getPower().toString());
+                let CatStore: any | null = newObjectCat.getFromStorage(newObjectCat.getType());
+                if (CatStore !== null) {
+                    CatStore.push(newCat);
+                    console.log('typeof(CatStore)', typeof (CatStore), CatStore)
+                    newObjectCat.writeToStorage(CatStore, newObjectCat.getType())
+                } else {
+                    newObjectCat.writeToStorage([newCat], newObjectCat.getType())
+                }
+                break;
         }
     }
 }
@@ -112,5 +242,9 @@ function ShowLocal(): void {
         newP.appendChild(document.createTextNode(text))
         tag.appendChild(newP)
     });
+}
+
+function getAnimal(): void {
+
 }
 
